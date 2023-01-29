a97fa5f249f26cfa6a65f8599c6a23edcae5e9af
ssuperj <poqwer95@naver.com>
2023-01-29 13:04:46 +0900

    fininsh project

--- src/server.js
+++ src/server.js
@@ -22,6 +22,7 @@ wsServer.on("connection", (socket) => {
   socket.on("enter_room", (roomNum, done) => {
     socket.join(roomNum);
     done();
+    socket.to(roomNum).emit("welcome");
   });
   socket.on("change_nick", (nickname, done) => {
     socket["nickname"] = nickname;
@@ -31,7 +32,24 @@ wsServer.on("connection", (socket) => {
     socket.to(roomNum).emit("new_message", `${socket.nickname}: ${message}`);
     done(`You: ${message}`);
   });
+  socket.on("offer", (offer, roomNum) => {
+    socket.to(roomNum).emit("offer", offer);
+  });
+  socket.on("answer", (answer, roomNum) => {
+    socket.to(roomNum).emit("answer", answer);
+  });
+  socket.on("ice", (ice, roomNum) => {
+    socket.to(roomNum).emit("ice", ice);
+  });
 });
 
+// wsServer.on("connection", (socket) => {
+//   socket.on("join_room", (roomName, done) => {
+//     socket.join(roomName);
+//     done();
+//     socket.to(roomName).emit("welcome");
+//   });
+// });
+
 // server.listen(process.env.PORT);
-httpServer.listen(3000);
+httpServer.listen(3100);
